0b9ff75814935da1f93d89ba34a5e1796734e227
Subhadip Some <some25377@gmail.com>
2025-11-09 19:52:47 +0530

    Add files via upload

---
+++ dsadb.js
@@ -0,0 +1,50 @@
+const problemsDB = {
+    easy: [
+        "Second Largest Element",
+        "Third Largest Element",
+        "Reverse an Array",
+        "Reverse Array in Groups",
+        "Rotate Array",
+        "Three Great Candidates",
+        "Max Consecutive Ones",
+        "Move All Zeroes to End",
+        "Wave Array",
+        "Stock Buy and Sell - One Transaction",
+        "Stock Buy and Sell - Multiple Transactions",
+        "Remove Duplicates from Sorted Array",
+        "Palindrome Check",
+        "Reverse a String",
+        "Reverse Words",
+        "Check for Rotation",
+        "Roman to integer"
+    ],
+
+    medium: [
+        "Next Permutation",
+        "Majority Element",
+        "Majority Element II",
+        "Minimize the Heights II",
+        "Maximum Subarray Sum",
+        "Maximum Product Subarray",
+        "Product of Array Except Self",
+        "Subarray with Product Less Than K",
+        "Integer to Words",
+        "Fizz Buzz",
+        "Palindromic Sentence Check",
+        "Isomorphic Strings"
+    ],
+
+    hard: [
+        "Trapping Rain Water",
+        "Maximum Circular Subarray Sum",
+        "Smallest Missing Positive Number",
+        "Jump Game",
+        "Closest Subsequence Sum",
+        "Smallest Non-Representable Sum",
+        "Repeatedly Remove Duplicates",
+        "Multiply Two Strings",
+        "Search Pattern (KMP Algorithm)",
+        "Search Pattern (Rabin-Karp Algorithm)",
+        "Shortest Common Supersequence"
+    ]
+};
